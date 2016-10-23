b628c49c3e2eeac4a405ff882f381a7b54aa9947
Colm Tuite <colmtuite@gmail.com>
2016-10-23 22:03:10 +0100

    Link component

---
+++ components/Link.jsx
@@ -0,0 +1,13 @@
+const sharedClasses = {
+    'fuck',
+    'off',
+    'you',
+    'cunt'
+};
+
+<a
+    className="{sharedClasses}"
+    href={props.url}
+    data-id={props.id}>
+    {props.text}
+</a>
---
+++ components/Root.jsx
@@ -0,0 +1,8 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <Link url="http://www.fuckoff.com" id="14" text="Fuck off you cunt" />
+    </body>
+</html>
